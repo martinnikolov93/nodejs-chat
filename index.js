cb335ea371ae63c41d61dd617e3b65c6483178b3
MartinNikolov <martin.v.nikolov93@gmail.com>
2020-08-06 20:55:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ mongoose.connect(dbUrl, {
     console.log('Database is setup and running')
 })
 
-const server = http.listen(80, () => {
+const server = http.listen(443, () => {
     console.log('Server is running on port', server.address().port);
 });
